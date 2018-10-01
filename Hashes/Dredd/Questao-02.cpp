4d538e083b2f1ab8f63ad5cda847c876b7461ffa
Lucas Neves <lucas54neves@gmail.com>
2018-10-01 20:54:02 -0300

    Exercícios para serem feitos de Hashes

---
+++ Hashes/Dredd/Questao-02.cpp
@@ -0,0 +1,142 @@
+// Tabela Hash com tratamento de colisão por endereçamento aberto
+#include <iostream>
+#include <exception>
+
+typedef int TValor; // tipo do valor armazenado
+
+class InfoHash {
+    friend class Hash; // essa classe só deve ser usada na classe Hash
+    InfoHash();
+    InfoHash(const std::string& chave, const TValor& valor);
+    std::string mChave;
+    TValor mValor;
+};
+
+class Hash {
+    public:
+        Hash(unsigned capacidade = 50);
+        ~Hash();
+        // Mostra todos as posições de armazenamento da hash.
+        void EscreverEstrutura(std::ostream& saida) const;
+        // Insere uma cópia do valor. Não permite inserção de chave repetida.
+        void Inserir(const std::string& chave, const TValor& valor);
+        // Remove um item da hash associado com a chave dada.
+        void Remover(const std::string& chave);
+        // Retorna o valor associado a uma chave.
+        TValor Valor(const std::string& chave) const;
+    protected:
+        // Retorna a posicao em que uma chave está armazenada na estrutura.
+        unsigned Buscar(const std::string& chave) const;
+        // Retorna a posicao em que uma chave deveria ficar na estrutura.
+        unsigned Posicao(const std::string& chave) const;
+
+        InfoHash** mVetPtDados;
+        InfoHash* REMOVIDO; // ponteiro especial a ser usado para marcar posições de elemento removido
+        unsigned mCapacidade;
+        unsigned mTamanho;
+};
+
+using namespace std;
+
+InfoHash::InfoHash()
+    : mChave(), mValor() {
+}
+
+InfoHash::InfoHash(const std::string& chave, const TValor& valor)
+    : mChave(chave), mValor(valor) {
+}
+
+Hash::Hash(unsigned capacidade) // capacidade tem valor default
+    : mVetPtDados(new InfoHash*[capacidade]), REMOVIDO(new InfoHash()), mCapacidade(capacidade), mTamanho(0) {
+    // FALTA FAZER:
+    // inicializar todas as posições de armazenamento com NULL indicando posição VAZIA
+}
+
+Hash::~Hash() {
+    // FALTA FAZER:
+    // desalocar memória de cada item (InfoHash) armazenado
+    // desalocar o ponteiro especial REMOVIDO
+    // desalocar o vetor de ponteiros
+}
+
+unsigned Hash::Buscar(const std::string& chave) const {
+    // Retorna a posicao em que uma chave está armazenada na estrutura. Protegido.
+}
+
+void Hash::EscreverEstrutura(std::ostream& saida) const {
+    // Mostra todos as posições de armazenamento da hash. Ajuda a debugar.
+    for (unsigned i = 0; i < mCapacidade; ++i) {
+        saida << '[' << i;
+        if (mVetPtDados[i] != NULL) {
+            if (mVetPtDados[i] == REMOVIDO)
+                saida << "/removido";
+            else
+                saida << '/' << mVetPtDados[i]->mChave << '/' << mVetPtDados[i]->mValor;
+        }
+        saida << "] ";
+    }
+}
+
+void Hash::Inserir(const string& chave, const TValor& valor) {
+    // Insere uma cópia do valor. Não permite inserção de chave repetida.
+}
+
+unsigned Hash::Posicao(const string& chave) const {
+    // Retorna a posição de armazenamento de uma chave, 0 <= posicao < mCapacidade. Protegido.
+    unsigned pos = 1;
+    unsigned tamanho = unsigned(chave.length());
+    for (unsigned i = 0; i < tamanho; ++i)
+        pos = 7 * pos + chave[i];
+    return pos % mCapacidade;
+}
+
+void Hash::Remover(const std::string& chave) {
+    // Remove um item da hash associado com a chave dada.
+}
+
+TValor Hash::Valor(const std::string& chave) const {
+    // Retorna o valor associado a uma chave.
+}
+
+int main() {
+    unsigned capacidade;
+    cin >> capacidade;
+    Hash tabela(capacidade);
+    char operacao;
+    string chave;
+    TValor valor;
+    do {
+        try {
+            cin >> operacao;
+            switch (operacao) {
+                case 'i': // inserir
+                    cin >> chave >> valor;
+                    tabela.Inserir(chave, valor);
+                    break;
+                case 'r': // remover
+                    cin >> chave;
+                    tabela.Remover(chave);
+                    break;
+                case 'c': // consultar
+                    cin >> chave;
+                    valor = tabela.Valor(chave);
+                    cout << valor << endl;
+                    break;
+                case 'd': // debug (mostrar estrutura)
+                    tabela.EscreverEstrutura(cout);
+                    cout << endl;
+                    break;
+                case 'f': // finalizar
+                    // vai testar depois
+                    break;
+                default:
+                    cerr << "operação inválida\n";
+            }
+        }
+        catch (exception& e) {
+            // ignorar a mensagem que explica o erro e só escrever ERRO.
+            cout << "ERRO" << endl;
+        }
+    } while (operacao != 'f');
+    return 0;
+}
---
+++ Hashes/Dredd/Questao-03.cpp
@@ -0,0 +1,162 @@
+// Tabela Hash com tratamento de colisão por endereçamento aberto
+#include <iostream>
+#include <string>
+#include <exception>
+
+typedef int TValor; // tipo do valor armazenado
+
+class InfoHash {
+    friend class Hash; // essa classe só deve ser usada na classe Hash
+    InfoHash();
+    InfoHash(const std::string& chave, const TValor& valor);
+    std::string mChave;
+    TValor mValor;
+};
+
+class Hash {
+    public:
+        Hash(unsigned capacidade = 50);
+        ~Hash();
+        // Mostra todos as posições de armazenamento da hash.
+        void EscreverEstrutura(std::ostream& saida) const;
+        // Insere uma cópia do valor. Não permite inserção de chave repetida.
+        void Inserir(const std::string& chave, const TValor& valor);
+        // Remove um item da hash associado com a chave dada.
+        void Remover(const std::string& chave);
+        // Retorna o valor associado a uma chave.
+        TValor Valor(const std::string& chave) const;
+    protected:
+        // Retorna a posicao em que uma chave está armazenada na estrutura.
+        unsigned Buscar(const std::string& chave) const;
+        // Insere um InfoHash na estrutura. Auxiliar para Inserir(chave/valor) e Redimensionar.
+        void Inserir(InfoHash* ptPar);
+        // Retorna a posicao em que uma chave deveria ficar na estrutura.
+        unsigned Posicao(const std::string& chave) const;
+        // Altera a capacidade da estrutura.
+        void Redimensionar(unsigned novaCapacidade);
+        InfoHash** mVetPtDados;
+        InfoHash* REMOVIDO; // ponteiro especial a ser usado para marcar posições de elemento removido
+        unsigned mCapacidade;
+        unsigned mTamanho;
+};
+
+using namespace std;
+
+InfoHash::InfoHash()
+    : mChave(), mValor() {
+}
+
+InfoHash::InfoHash(const std::string& chave, const TValor& valor)
+    : mChave(chave), mValor(valor) {
+}
+
+Hash::Hash(unsigned capacidade) // capacidade tem valor default
+    : mVetPtDados(new InfoHash*[capacidade]), REMOVIDO(new InfoHash()), mCapacidade(capacidade), mTamanho(0) {
+    // FALTA FAZER:
+    // inicializar todas as posições de armazenamento com NULL indicando posição VAZIA
+    #warning Construtor não implementado
+}
+
+Hash::~Hash() {
+    // FALTA FAZER:
+    // desalocar memória de cada item (InfoHash) armazenado
+    // desalocar o ponteiro especial REMOVIDO
+    // desalocar o vetor de ponteiros
+    #warning Destrutor não implementado
+}
+
+unsigned Hash::Buscar(const std::string& chave) const {
+    // Retorna a posicao em que uma chave está armazenada na estrutura. Protegido.
+}
+
+void Hash::EscreverEstrutura(std::ostream& saida) const {
+    // Mostra todos as posições de armazenamento da hash. Ajuda a debugar.
+    for (unsigned i = 0; i < mCapacidade; ++i) {
+        saida << '[' << i;
+        if (mVetPtDados[i] != NULL) {
+            if (mVetPtDados[i] == REMOVIDO)
+                saida << "/removido";
+            else
+                saida << '/' << mVetPtDados[i]->mChave << '/' << mVetPtDados[i]->mValor;
+        }
+        saida << "] ";
+    }
+}
+
+void Hash::Inserir(const string& chave, const TValor& valor) {
+    // Insere uma cópia do valor. Não permite inserção de chave repetida.
+}
+
+void Hash::Inserir(InfoHash* ptPar) {
+    // Insere um par (chave/valor) na estrutura. Supõe que a chave é válida e única.
+    // Supõe que há espaço para inserção e que o ponteiro é válido. Não altera o tamanho.
+    // Método protegido, auxiliar para Inserir(chave, valor) e Redimensionar(cap).
+    #warning Inserir(InfoHash*) não implementado
+}
+
+unsigned Hash::Posicao(const string& chave) const {
+    // Retorna a posição de armazenamento de uma chave, 0 <= posicao < mCapacidade. Protegido.
+    unsigned pos = 1;
+    unsigned tamanho = unsigned(chave.length());
+    for (unsigned i = 0; i < tamanho; ++i)
+        pos = 7 * pos + chave[i];
+    return pos % mCapacidade;
+}
+
+void Hash::Redimensionar(unsigned novaCapacidade) {
+    // Altera a capacidade da estrutura. Pode ser chamado mesmo que a estrutura não esteja cheia.
+    // Verifica se a novaCapacidade é válida. Método protegido.
+    #warning Redimensionamento não implementado
+}
+
+void Hash::Remover(const std::string& chave) {
+    // Remove um item da hash associado com a chave dada.
+    #warning Remoção não implementada
+}
+
+TValor Hash::Valor(const std::string& chave) const {
+    // Retorna o valor associado a uma chave.
+}
+
+int main() {
+    unsigned capacidade;
+    cin >> capacidade;
+    Hash tabela(capacidade);
+    char operacao;
+    string chave;
+    TValor valor;
+    do {
+        try {
+            cin >> operacao;
+            switch (operacao) {
+                case 'i': // inserir
+                    cin >> chave >> valor;
+                    tabela.Inserir(chave, valor);
+                    break;
+                case 'r': // remover
+                    cin >> chave;
+                    tabela.Remover(chave);
+                    break;
+                case 'c': // consultar
+                    cin >> chave;
+                    valor = tabela.Valor(chave);
+                    cout << valor << endl;
+                    break;
+                case 'd': // debug (mostrar estrutura)
+                    tabela.EscreverEstrutura(cout);
+                    cout << endl;
+                    break;
+                case 'f': // finalizar
+                    // vai testar depois
+                    break;
+                default:
+                    cerr << "operação inválida\n";
+            }
+        }
+        catch (exception& e) {
+            // ignorar a mensagem que explica o erro e só escrever ERRO.
+            cout << "ERRO" << endl;
+        }
+    } while (operacao != 'f');
+    return 0;
+}
