3d0334ceba1bf2aebc2d93da42b72919010527e3
Lucas Neves <lucas54neves@gmail.com>
2018-10-11 11:27:48 -0300

    Hash e Heap - Questão 6 - Nenhum erro encontrado

--- Hashes-e-Heaps/Dredd/questao-06.cpp
+++ Hashes-e-Heaps/Dredd/questao-06.cpp
@@ -6,28 +6,96 @@ typedef int Data;
 
 class MinHeap {
     public:
-        MinHeap();
+        MinHeap(Data vet[], int tam);
         ~MinHeap();
-        void Inserir(Data d);
-        void Imprimir();
-        Data EspiarRaiz();
-        Data RetirarRaiz();
+        void Imprimir(int pos);
     private:
         Data* mVetDados;
-        unsigned int mTamanho;
-        unsigned int mCapacidade;
-        unsigned int Pai(unsigned int i);
-        unsigned int Esquerdo(unsigned int i);
-        unsigned int Direito(unsigned int i);
-        void CorrigirSubindo(unsigned int i);
-        void CorrigirDescendo(unsigned int i);
+        int mTamanho;
+        int mCapacidade;
+        int Pai(int i);
+        int Esquerdo(int i);
+        int Direito(int i);
+        void CorrigirDescendo(int i);
         void Arrumar();
 };
 
-MinHeap::MinHeap() {
+MinHeap::MinHeap(Data vet[], int tam) {
+    mTamanho = tam;
+    mVetDados = new Data[tam];
     
+    for (int i = 0; i < mTamanho; ++i) {
+        mVetDados[i] = vet[i];
+    }
+    
+    Arrumar();
 }
 
 MinHeap::~MinHeap() {
+    delete[] mVetDados;
+}
+
+int MinHeap::Pai(int i) {
+    return (i-1)/2;
+}
+
+int MinHeap::Esquerdo(int i) {
+    return (2*i+1);
+}
+
+int MinHeap::Direito(int i) {
+    return (2*i+2);
+}
+
+void MinHeap::Arrumar() {
+    for (int i = (mTamanho/2-1); i >= 0; --i) {
+        CorrigirDescendo(i);
+        Imprimir(i);
+    }
+}
+
+void MinHeap::CorrigirDescendo(int i) {
+    int esq = Esquerdo(i);
+    int dir = Direito(i);
+    int menor;
     
+    if (esq < mTamanho && mVetDados[esq] < mVetDados[i]) {
+        menor = esq;
+    } else {
+        menor = i;
+    }
+    
+    if (dir < mTamanho && mVetDados[dir] < mVetDados[menor]) {
+        menor = dir;
+    }
+    
+    if (menor != i) {
+        swap(mVetDados[menor], mVetDados[i]);
+        CorrigirDescendo(menor);
+    }
+}
+
+void MinHeap::Imprimir(int pos) {
+    cout << pos << ": ";
+    for (int i = 0; i < mTamanho; ++i) {
+        cout << mVetDados[i];
+        
+        if (i < mTamanho - 1) {
+            cout << " ";
+        } else {
+            cout << endl;
+        }
+    }
+}
+
+int main() {
+    int tam;
+    cin >> tam;
+    
+    int vet[tam];
+    for (int i = 0; i < tam; ++i) {
+        cin >> vet[i];
+    }
+    
+    MinHeap(vet, tam);
 }
